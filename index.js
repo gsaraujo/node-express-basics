b90e8022f07dae69e64c96f8a2acb6e369afad6c
Gustavo <sasaeucomvoce@hotmail.com>
2024-02-10 23:02:50 -0300

    feat: testing new route to list users

--- index.js
+++ index.js
@@ -5,7 +5,34 @@ let server = http.createServer((req,res)=> {
     console.log('URL: ' + req.url);
     console.log('METHOD:', req.method);
 
-    res.end('OK');
+    switch(req.url) {
+        case '/':
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'text/html');
+            res.end('<h1>Olá Mundo</h1>');
+
+            break;
+        case '/users':
+
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.end(JSON.stringify({
+                users:[{
+                    name: 'Alita',
+                    email: 'alita@angel.com',
+                    id: 1
+
+                }]
+            }));
+
+
+            break;
+        case 'a':
+            break;
+        case 'a':
+            break;
+
+    }
 
 });
 
