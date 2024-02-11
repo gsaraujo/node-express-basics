fa00608c360681a9944317f590cf89b342f35fc9
Gustavo <sasaeucomvoce@hotmail.com>
2024-02-10 23:44:51 -0300

    feat: using express to create the server

--- index.js
+++ index.js
@@ -1,42 +1,34 @@
-const http = require('http');
+const express = require('express');
 
-let server = http.createServer((req,res)=> {
+let app = express();
+
+app.get('/', (req,res)=> {
+
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('<h1>Olá Mundo</h1>');
 
     console.log('URL: ' + req.url);
     console.log('METHOD:', req.method);
+});
 
-    switch(req.url) {
-        case '/':
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end('<h1>Olá Mundo</h1>');
+app.get('/users', (req,res)=> {
 
-            break;
-        case '/users':
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'application/json');
+    res.json({
+        users:[{
+            name: 'Alita',
+            email: 'alita@angel.com',
+            id: 1
 
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'application/json');
-            res.end(JSON.stringify({
-                users:[{
-                    name: 'Alita',
-                    email: 'alita@angel.com',
-                    id: 1
-
-                }]
-            }));
-
-
-            break;
-        case 'a':
-            break;
-        case 'a':
-            break;
-
-    }
+        }]
+    });
 
 });
 
-server.listen(3000, '127.0.0.1', ()=>{
+
+app.listen(3000, '127.0.0.1', ()=>{
 
     console.log('Server up!');
 
