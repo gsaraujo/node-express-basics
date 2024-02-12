d8ced38cc24f87011ed25c522f7f892dacfb40ad
Gustavo <sasaeucomvoce@hotmail.com>
2024-02-11 22:57:04 -0300

    feat: list users on the db

--- routes/users.js
+++ routes/users.js
@@ -8,15 +8,22 @@ module.exports = (app)=>{
 
     app.get('/users', (req,res)=> {
 
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'application/json');
-        res.json({
-            users:[{
-                name: 'Alita',
-                email: 'alita@angel.com',
-                id: 1
+        db.find({}).sort({name:1}).exec((err, users)=>{//parameter on find -> object that tells what you are looking for. Since we need all the users on the db, we use an empty object as a parameter
 
-            }]
+            if(err){
+                console.log(`error: ${err}`);
+                res.status(400).json({
+                    error:err
+                });
+            } else {
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'application/json');
+                res.json({
+                    users// before ECMA 6 -> users: users. Now, if the key has the same name as the variable, you only need to write once.
+                });
+
+            }
+
         });
 
     });
